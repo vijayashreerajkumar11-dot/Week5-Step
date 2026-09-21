c1d6e27607b87be1884dbd8ef1f257fc803795c9
vijayashree <vijayashreerajkumar11@gmail.com>
2026-09-21 21:11:31 +0530

    Add Week 5 OOP placement record problem

---
+++ Problem1.java
@@ -0,0 +1,30 @@
+class PlacementRecord {
+    String studentName;
+    String company;
+    double packageLpa;
+
+    PlacementRecord(String studentName, String company, double packageLpa) {
+        this.studentName = studentName;
+        this.company = company;
+        this.packageLpa = packageLpa;
+    }
+
+    void printRecord() {
+        System.out.println(studentName + " -> " + company + " @ " + packageLpa + " LPA");
+    }
+}
+
+public class Problem1 {
+    public static void main(String[] args) {
+
+        PlacementRecord p1 = new PlacementRecord("Ravi", "TCS", 4.5);
+        PlacementRecord p2 = new PlacementRecord("Anitha", "Zoho", 6.2);
+        PlacementRecord p3 = new PlacementRecord("Karthik", "Infosys", 4.0);
+
+        PlacementRecord[] records = {p1, p2, p3};
+
+        for (PlacementRecord record : records) {
+            record.printRecord();
+        }
+    }
+}
